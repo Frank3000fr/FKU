c922edc2b08e14fcbb95767e93dbe8d9cf85dc59
Frank3000fr <a0936506108@gmail.com>
2021-06-28 00:03:20 +0800

    g

--- FKK.py
+++ FKK.py
@@ -5,3 +5,15 @@ print(4)
 print(5)
 print(6)
 print(7)
+
+x = 4
+while x < 10:
+    print('還在迴圈')
+    print(x)
+    x = x+1
+
+print('出來了!!!!!!!')
+print('出來了!!!!!!!')
+print('出來了!!!!!!!')
+print('出來了!!!!!!!')
+print('出來了!!!!!!!')
---
+++ 連線至GMAIL寄信.py
@@ -0,0 +1,33 @@
+# 1.使用email.message模組建立內容
+# 2.使用smtplib模組發送信件
+# 3.驗證寄件人身分(帳號密碼或兩階段認證)
+
+# 準備訊息物件設定
+# 載入模組
+import smtplib
+import email.message
+# 建立訊息物件
+msg = email.message.EmailMessage()
+# 利用物件建立基本設定
+
+from_a = input("請輸入寄件人信箱：")
+to_b = input("請輸入收件人信箱：")
+
+
+msg["From"] = from_a
+msg["To"] = to_b
+msg["Subject"] = "你好"
+
+# 寄送郵件主要內容
+# msg.set_content("測試郵件純文字內容") #純文字信件內容
+msg.add_alternative("<h3>HTML內容</h3>安安這是寄送郵件測試", subtype="html")  # HTML信件內容
+
+acc = input("請輸入gmail帳號：")
+password = input("請輸入密碼")
+
+# 連線到SMTP Sevver
+# 可以從網路上找到主機名稱和連線埠
+server = smtplib.SMTP_SSL("smtp.gmail.com", 465)  # 建立gmail連驗
+server.login(acc, password)
+server.send_message(msg)
+server.close()  # 發送完成後關閉連線
